292acf802755378cc4979bd9644d4b780c3fba64
Robin Turner <robinianturner@gmail.com>
2017-11-15 18:33:32 +0000

    Add grid and clear grid feature

--- scripts.js
+++ scripts.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
 
+
+//NEED TO GET VALUE OF INPUT - NOT WORKING
+	// const numInput = parseInt($('#number').val());
+	// console.log(numInput);
+
+
+	const button = $('#btn');
+
+	$('button').on('click', clearGrid);
+
+	//Create squares grid
 	for(let i=0; i < 16; i++){
-		$('.squares').append('<div class="square">Alright</div>');
+		for(let j=0; j < 16; j++){
+			$('#squares').append('<div class="square"></div>')[j];
+		}
 	}
 	//Change square colour once hovered over
 	$('.square').on('mouseenter', function(){
 		$(this).addClass('changed');
 	});
+
+	//Clear Grid
+	function clearGrid(){
+		$('.square').removeClass('changed');
+	}
 
 });
