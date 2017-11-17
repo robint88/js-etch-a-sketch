d7567c76bc20e99493d6a2ca1e8bd9102dd9f677
Robin Turner <robinianturner@gmail.com>
2017-11-17 17:52:24 +0000

    Complete basic functionality

--- scripts.js
+++ scripts.js
@@ -1,25 +1,33 @@
 $(document).ready(function(){
 
-
 	const btn = $('#btn');
 	const reset = $('#reset');
+	let gridSize = 16;
 
 	$(btn).on('click', clearGrid);
-	$(reset).on('click', resetSize);
-
-
-	//Create squares grid
-	for(let i=0; i < 16; i++){
-		for(let j=0; j < 16; j++){
-			$('#squares').append('<div class="square"></div>')[j];
-		}
-	}
-	//Change square colour once hovered over
-	$('.square').on('mouseenter', function(){
-		$(this).addClass('changed');
-	});
+	$(reset).on('click', resetGridSize);
+	makeGrid(gridSize);
 
 	//------FUNCTIONS------//
+
+	//Create squares grid
+	function makeGrid(size) {
+
+		for(let i=0; i < size; i++){
+			let row = '<div class="row">';
+			for(let j=0; j < size; j++){
+				row+= '<div class="square"></div>';
+			}
+			row += '</div>';
+			$('#squares').append(row);
+		}
+		let squareSize = ((420 - (gridSize * 2))  / gridSize) + 'px';
+		$('.square').css({'height': squareSize, 'width': squareSize});
+		//Change colour on hover
+		$('.square').on('mouseenter', function(){
+			$(this).addClass('changed');
+		});
+	}
 
 	//Clear Grid
 	function clearGrid(){
@@ -27,12 +35,23 @@ $(document).ready(function(){
 	}
 
 	//Change grid size
-	function resetSize(){
-		$('#reset').on('click', function(){
+	function resetGridSize(){
+		gridSize = prompt('Input a number greater than 16');
+			if(gridSize >= 16){
 			//Reset grid size
-			let popup = prompt('What size grid? (Number must be greater than 16)');
-			console.log(popup);
-		});
+			$('.square').remove();
+			console.log(gridSize);
+			makeGrid(gridSize);
+		} else {
+			notLarge();
+		}
 	}
+
+	// Grid size less than 16
+	function notLarge(){
+		alert('That number is lower than 16! Try again');
+		resetGridSize();
+	}
+
 
 });
